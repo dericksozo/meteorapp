a25f6dbf55e54f81c8514506ab6f6731ef747f1e
Derick Ruiz <derickiscool@gmail.com>
2015-10-11 12:34:43 -0500

    Events on emojis and making them display.

--- client/index.js
+++ client/index.js
@@ -2,6 +2,25 @@ Meteor.subscribe("emoji");
 
 Template.emojiSelector.helpers({
     getEmojiList: function () {
-        return Emoji.find({category: 'places'});
+        var category = Session.get('category') || 'places';
+        return Emoji.find({category: category});
+    },
+    selectedEmojis: function () {
+        var selectedEmoji = Session.get('selectedEmoji') || '';
+        return selectedEmoji;
+    }
+});
+
+Template.emojiSelector.events({
+    'click .js-change-category': function (event) {
+        event.preventDefault();
+        var category = event.toElement.dataset.category;
+        Session.set('category', category);
+    },
+    'click .emoji': function (event) {
+        var selectedEmoji = Session.get('selectedEmoji');
+        selectedEmoji = selectedEmoji + this.shortname + ' ';
+        Session.set('selectedEmoji', selectedEmoji);
+        console.log(Session.get('selectedEmoji'));
     }
 });
